3a169f5e2e38a36b3c852982ff775f782e339921
tarakuds <taradebby@gmail.com>
2022-06-19 18:48:44 +0100

    url modification

--- TaraKuds/TaraKuds/settings.py
+++ TaraKuds/TaraKuds/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(-@2rvp*2b1s-200ug^d3szc2mog%_h0h%66o^h&5_kj%4j0nc'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
